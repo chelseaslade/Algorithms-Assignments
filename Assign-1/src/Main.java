5fdc6a6e90634a22988ec7fba9db08b708f63e7b
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2025-01-13 16:00:18 -0330

    Init

---
+++ Assign-1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+//Calculate the average temperature, and give how many days are above the average temperature
+//1. Take an input from the user (eg. 5)
+//2. Prompt the user to enter all the 5 numbers (temperature values)
+//3. Calculate the average temperature
+//4. Given the average temperature, how many of the numbers in the line 2 are above the average temperature?
+
+
+
+        //User Input
+
+        //Calculate Average
+
+        //Return numbers above average
+    }
+}
