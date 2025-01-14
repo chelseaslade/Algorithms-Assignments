78f44bfff65004b28de17081def262c1444d4264
Chelsea Slade <chelsea.mayne@keyin.com>
2025-01-14 14:26:58 -0330

    Added scanner and init array

--- Assign-1/Main.java
+++ Assign-1/Main.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-//Calculate the average temperature, and give how many days are above the average temperature
-//1. Take an input from the user (eg. 5)
-//2. Prompt the user to enter all the 5 numbers (temperature values)
-//3. Calculate the average temperature
-//4. Given the average temperature, how many of the numbers in the line 2 are above the average temperature?
+        //Calculate the average temperature, and give how many days are above the average temperature
+        //1. Take an input from the user (eg. 5)
+        //2. Prompt the user to enter all the 5 numbers (temperature values)
+        //3. Calculate the average temperature
+        //4. Given the average temperature, how many of the numbers in the line 2 are above the average temperature?
 
         //User Input
+        Scanner sc = new Scanner(System.in);
+        int avgTemps[5];
+
+        System.out.println("Enter the first temperature: ");
+        int tempOne = sc.nextInt();
+        sc.nextLine();
+
 
         //Calculate Average
 
