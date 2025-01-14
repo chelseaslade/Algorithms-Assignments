b1d6b93b2cafc4f018553837e342e10aefaa1b8d
Chelsea Slade <chelsea.mayne@keyin.com>
2025-01-14 14:46:32 -0330

    Added average calculation

--- Assign-1/Main.java
+++ Assign-1/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,14 +11,38 @@ public class Main {
 
         //User Input
         Scanner sc = new Scanner(System.in);
-        int avgTemps[5];
+        int[] tempArray = new int[5];
 
         System.out.println("Enter the first temperature: ");
         int tempOne = sc.nextInt();
+        tempArray[0] = tempOne;
         sc.nextLine();
 
+        System.out.println("Enter the second temperature: ");
+        int tempTwo = sc.nextInt();
+        tempArray[1] = tempTwo;
+        sc.nextLine();
+
+        System.out.println("Enter the third temperature: ");
+        int tempThree = sc.nextInt();
+        tempArray[2] = tempThree;
+        sc.nextLine();
+
+        System.out.println("Enter the fourth temperature: ");
+        int tempFour = sc.nextInt();
+        tempArray[3] = tempFour;
+        sc.nextLine();
+
+        System.out.println("Enter the fifth temperature: ");
+        int tempFive = sc.nextInt();
+        tempArray[4] = tempFive;
+        sc.nextLine();
+
+        System.out.println(Arrays.toString(tempArray));
 
         //Calculate Average
+        int avgTemp = (Arrays.stream(tempArray).sum()) / tempArray.length;
+        System.out.println("Average Temperature: " + avgTemp);
 
         //Return numbers above average
     }
