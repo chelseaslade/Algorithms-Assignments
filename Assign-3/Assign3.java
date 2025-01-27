7250fff0d35da421102c7450d255287a72b5558d
Chelsea Slade <chelsea.mayne@keyin.com>
2025-01-27 15:29:30 -0330

    Start of assignment 3, files init

---
+++ Assign-3/Assign3.java
@@ -0,0 +1,13 @@
+public class Assign3 {
+    public static void main(String[] args) {
+        SingleLinkedList sll = new SingleLinkedList();
+        sll.insertInLinkedList(1,0);
+        sll.insertInLinkedList(2, 1);
+        sll.insertInLinkedList(3, 3);
+        sll.insertInLinkedList(4, 4);
+
+
+        sll.traverseLinkedList();
+        sll.searchNode(5);
+    }
+}
