fe8be1daf6bad8297ac9cba82d6088c19bbb2d17
Chelsea Slade <chelsea.mayne@keyin.com>
2025-01-28 09:42:12 -0330

    Finished delete method and added testing to main - complete

--- Assign-3/Assign3.java
+++ Assign-3/Assign3.java
@@ -1,13 +1,27 @@
 public class Assign3 {
     public static void main(String[] args) {
+        //Test traverse method
         SingleLinkedList sll = new SingleLinkedList();
         sll.insertInLinkedList(1,0);
         sll.insertInLinkedList(2, 1);
         sll.insertInLinkedList(3, 3);
         sll.insertInLinkedList(4, 4);
 
-
         sll.traverseLinkedList();
         sll.searchNode(5);
+
+        //Test delete method
+        SingleLinkedList sll2 = new SingleLinkedList();
+        sll2.createdLinkedList(10);
+        sll2.insertInLinkedList(20, 1);
+        sll2.insertInLinkedList(30, 2);
+        sll2.insertInLinkedList(40, 3);
+        sll2.traverseLinkedList(); // Output: 10 -> 20 -> 30 -> 40
+
+        sll2.deleteNode(0);
+        sll2.traverseLinkedList(); // Output: 20 -> 30 -> 40
+
+        sll2.deleteNode(2);
+        sll2.traverseLinkedList(); // Output: 20 -> 30
     }
 }
--- Assign-3/SingleLinkedList.java
+++ Assign-3/SingleLinkedList.java
@@ -64,9 +64,7 @@ public class SingleLinkedList {
         }
     }
 
-
     // search for a Node
-
     public boolean searchNode (int nodeValue) {
         if (head != null) {
             Node tempNode = head;
@@ -82,10 +80,60 @@ public class SingleLinkedList {
         return false;
     }
 
-
+    //Assignment 3:
     //Deleting a Node from the linked list
-    //0. if the link doesn't
-    //1. delete at the begining
+    //0. if the link doesn't exist
+    //1. delete at the beginning
     //2. delete at the ending
     //3. delete anywhere
+
+    public void deleteNode(int nodeLocation) {
+        // If the linked list does not exist
+        if (head == null) {
+            System.out.println("Node does not exist. Cannot delete.");
+            return;
+        }
+
+        // Delete at the beginning
+        if (nodeLocation == 0) {
+            head = head.next;
+            size--;
+            if (size == 0) {
+                tail = null;
+            }
+            System.out.println("Node at position " + nodeLocation + " has been deleted.");
+            return;
+        }
+
+        // Delete at the end
+        else if (nodeLocation >= size - 1) {
+            Node tempNode = head;
+            for (int i = 0; i < size - 2; i++) {
+                tempNode = tempNode.next;
+            }
+            if (tempNode == head) {
+                head = null;
+                tail = null;
+                size--;
+                System.out.println("Node at position " + nodeLocation + " has been deleted.");
+                return;
+            }
+            tempNode.next = null;
+            tail = tempNode;
+            size--;
+            System.out.println("Node at position " + nodeLocation + " has been deleted.");
+            return;
+        }
+
+        // Delete anywhere (middle nodes)
+        else {
+            Node tempNode = head;
+            for (int i = 0; i < nodeLocation - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            size--;
+            System.out.println("Node at position " + nodeLocation + " has been deleted.");
+        }
+    }
 }
